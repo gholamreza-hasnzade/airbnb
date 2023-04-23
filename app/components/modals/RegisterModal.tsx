3b1081a4d0387feff10edd013219ddfbd265aba9
mrclevercoder <frontenddev5000@gmail.com>
2023-04-23 11:07:59 +0330

    create input components

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -7,6 +7,8 @@ import { AiFillGithub } from "react-icons/ai";
 import { FcGoogle } from "react-icons/fc";
 import useRegisterModal from "@/app/hooks/useRegisterModal";
 import Modal from "./Modal";
+import Heading from "../navbar/Heading";
+import Input from "../inputs/Input";
 
 const RegisterModal = () => {
   const registerModal = useRegisterModal();
@@ -40,6 +42,36 @@ const RegisterModal = () => {
       });
   };
 
+  const bodyContant = (
+    <div className="flex flex-col gap-4">
+      <Heading title="Welcome to airbnb" subtitle="Create an account" />
+      <Input
+        id="email"
+        label="Email"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id="name"
+        label="Name"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id="password"
+        type="password"
+        label="Password"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+    </div>
+  );
   return (
     <Modal
       disabled={isLoading}
@@ -48,6 +80,7 @@ const RegisterModal = () => {
       actionLable="Continue"
       onClose={registerModal.onClose}
       onSubmit={handleSubmit(onSubmit)}
+      body={bodyContant}
     />
   );
 };
